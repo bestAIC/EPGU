8cecd8e7212746b2a79b6f108e444fac851ae27b
dibrivnyy <i.ya.kadet@gmail.com>
2014-07-22 18:22:45 +0400

    ad@aic.ru
    обратная связь (боковое меню для мобильной версии)

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
   SwitchPoint();  
   PlaceSmall();
   MenuShow();
+  MenuShowPhone();
 });
 
 
@@ -187,3 +188,31 @@ $('#menu_btn').click(function(event){
 })
 	
 }
+
+
+// Top menu mobile
+
+function MenuShowPhone(){
+  $('html').click(function(){
+    $('#menu_top_btn').addClass('closed').removeClass('opened');
+  })
+
+  $('.exit_phone').on('click', function(){
+    event.stopPropagation();
+    if ($('#menu_top_btn').hasClass('closed')) {
+      $('#menu_top_btn').addClass('opened').removeClass('closed').animate({right: '0px'});
+      $('#wrap').animate({left: '-244px'}); 
+    } else {
+      $('#menu_top_btn').addClass('closed').removeClass('opened').animate({right: '-244px'}); 
+      $('#wrap').animate({left: '0'});
+    }
+  })
+
+  $(window).on('resize', function(){
+    $('#menu_top_btn').css({right: '0'});
+  })
+}
+
+
+
+
