ba3dcd21bc4000e5900b304415b5aacfbb36865c
Phys <phys@aic.ru>
2014-07-22 18:54:53 +0400

    ФОС мобильная (правки)
    Signed-off-by: Phys <phys@aic.ru>

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
   SwitchPoint();  
   PlaceSmall();
   MenuShow();
-  MenuShowPhone();
 });
 
 
@@ -187,30 +186,6 @@ $('#menu_btn').click(function(event){
 	event.stopPropagation();
 })
 	
-}
-
-
-// Top menu mobile
-
-function MenuShowPhone(){
-  $('html').click(function(){
-    $('#menu_top_btn').addClass('closed').removeClass('opened');
-  })
-
-  $('.exit_phone').on('click', function(){
-    event.stopPropagation();
-    if ($('#menu_top_btn').hasClass('closed')) {
-      $('#menu_top_btn').addClass('opened').removeClass('closed').animate({right: '0px'});
-      $('#wrap').animate({left: '-244px'}); 
-    } else {
-      $('#menu_top_btn').addClass('closed').removeClass('opened').animate({right: '-244px'}); 
-      $('#wrap').animate({left: '0'});
-    }
-  })
-
-  $(window).on('resize', function(){
-    $('#menu_top_btn').css({right: '0'});
-  })
 }
 
 
--- js/mobile_main.js
+++ js/mobile_main.js
@@ -1,6 +1,7 @@
 // Inits
 $(document).ready(function() {
-  RetinaDisplay();  
+  RetinaDisplay(); 
+  MenuShowMobile();   
 });
 
 
@@ -24,5 +25,30 @@ function RetinaDisplay() {
 */
   
 
+}
+
+// Top menu mobile
+
+function MenuShowMobile(){
+  $('html').click(function(){
+    $('#menu_top_btn').addClass('closed').removeClass('opened');
+  })
+/*
+  $('.exit_phone').on('click', function(){
+    event.stopPropagation();
+    if ($('#menu_top_btn').hasClass('closed')) {
+      $('#menu_top_btn').addClass('opened').removeClass('closed').animate({right: '0px'});
+      $('#wrap').animate({left: '-244px'}); 
+    } else {
+      $('#menu_top_btn').addClass('closed').removeClass('opened').animate({right: '-244px'}); 
+      $('#wrap').animate({left: '0'});
+    }
+  })
+
+  $(window).on('resize', function(){
+    $('#menu_top_btn').css({right: '0'});
+  })
+*/  
+  
 }
 
