a1fdd7ef5ccd07a9d0ba748119787f94c62e7242
Phys <phys@aic.ru>
2014-08-05 18:03:49 +0400

    Исправление конфликта
    Signed-off-by: Phys <phys@aic.ru>

--- postanovka_na_registraciu.php
+++ postanovka_na_registraciu.php
@@ -13,7 +13,7 @@ include('include/head.php');
 
       <div id="content">
         <div class="title">
-          <span class="icomoon color_3">G</span><h1>Регистрация граждан</h1>
+          <span class="icomoon color_3">O</span><h1>Регистрация граждан</h1>
           <h2>по месту жительства</h2>
         </div>
         
@@ -240,6 +240,14 @@ include('include/head.php');
                   <option>Гражданство</option>                
                   <option>Белоруссия</option>
                   <option>Украина</option>
+                  <option>Белоруссия</option>
+                  <option>Украина</option>
+                  <option>Белоруссия</option>
+                  <option>Украина</option>
+                  <option>Белоруссия</option>
+                  <option>Украина</option>
+                  <option>Белоруссия</option>
+                  <option>Украина</option>
                 </select>                  
               </em><em class="wrapper l_33"><input type="text" placeholder="Дата вступления" class="big" /></em><br/>              
               <em class="wrapper l_100"><a class="btn white plus" href="#"><span class="icomoon">8</span><b>Добавить данные</b></a></em>               
@@ -358,28 +366,50 @@ include('include/head.php');
             </div>
             
             <h4>Просим вас выбрать удобное подразделение для подачи документов</h4>
-            <div class="map_cover clear">
-              <div class="find_result">
-                <h4>Доступные подразделения</h4>
-                <ul>
-                  <li>
-                    <span class="list_num">1</span>
-                    <h5>ИФНС России №1 по г.Москве</h5>
-                    <p>Москва, Ниглинная, 23</p>
+            
+            <div class="map_cover">
+              <div class="map_block" id="map"></div>
+              <div class="map_nav">
+                <h4 class="map_nav_h">Доступны подразделения:</h4>
+                <ul class="map_list">
+                  <li class="map_list_item">
+                    <p><a href="#"><span class="map_num">1</span><span class="map_txt">ИФНС России №1 по г.Москве</span></a></p>
+                    <p>Москва, Неглинная, 23</p>
                   </li>
-                  <li>
-                    <span class="list_num">2</span>
-                    <h5>ИФНС России №1 по г.Москве</h5>
-                    <p>Москва, Ниглинная, 23</p>
+                  <li class="map_list_item">
+                    <p><a href="#"><span class="map_num">2</span><span class="map_txt">МИФНС России №46 по г.Москве (Залы обслуживания № 3,4,5)</span></a></p>
+                    <p>В составе ИФНС России № 1 по г. Москве. Москва. Походный пр-д, 3, стр. 2</p>
                   </li>
-                  <li>
-                    <span class="list_num">3</span>
-                    <h5>ИФНС России №1 по г.Москве</h5>
-                    <p>Москва, Ниглинная, 23</p>
-                  </li>                                    
+                  <li class="map_list_item">
+                    <p><a href="#"><span class="map_num">3</span><span class="map_txt">ИФНС России № 34 по г. Москве</span></a></p>
+                    <p>Москва, Боткинский 2-й пр-д, 8, стр. 1</p>
+                  </li>
                 </ul>
-              </div>
-            </div>
+              </div><!-- map_nav -->
+
+              <div class="map_ballon_block">
+                <div class="map_ballon_wrap">
+                  <a class="map_ballon_close"><span class="icomoon">L</span></a>
+                  <div class="map_ballon_h">
+                    <p><strong>МИФНС России №46 по г. Москве</strong></p>
+                    <p>В составе ИФНС России № 1 по г. Москве</p>
+                  </div>
+                  <ul class="map_ballon_list">
+                    <li class="map_ballon_item">Адрес: Москва, Походный пр-д, 3, стр. 2</li>
+                    <li class="map_ballon_item">Режим работы: Пн-Пт: 9:00 - 18:00,</li>
+                    <li class="map_ballon_item">Обед: 12:30 - 13:30</li>
+                    <li class="map_ballon_item">Телефон: +7 (495) 913-00-09</li>
+                    <li class="map_ballon_item">Как добраться:</li>
+                    <li class="map_ballon_item">Ближайшее метро - Волоколамская,</li>
+                    <li class="map_ballon_item">Мякинино, Тушинская</li>
+                    <li class="map_ballon_item">E-mail: i010500@r01.nalog.ru</li>
+                  </ul>
+                </div>
+                <div class="map_placemark">
+                  <img src="svg/4.svg" alt="">
+                </div>
+              </div><!-- map_ballon_block -->
+            </div><!-- map_cover -->
                                                  
           </fieldset> 
 
