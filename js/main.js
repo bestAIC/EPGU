cf002656d2190810be3027925e460a1834b7066a
Phys <phys@aic.ru>
2014-08-25 11:50:11 +0400

    Правки синхронизация
    Signed-off-by: Phys <phys@aic.ru>

--- js/main.js
+++ js/main.js
@@ -16,7 +16,10 @@ function SForm() {
 
   $(function() {
     /*стилизация элементов*/
-    $('input, select').styler();
+    $('input, select').styler({
+      selectSearchLimit: 13,
+      selectSmartPositioning: false
+    });
     
     /*маска ввода даты*/    
     $(".format_date").mask("99.99.9999",{placeholder:"_"});
@@ -29,9 +32,9 @@ function SForm() {
       });
     });
     
-    /*выпадающие поля при выборе пункта*/ 
+     /*выпадающие поля при выборе пункта*/ 
     
-    $('.fields_cover .jq-radio').click(function(){      
+    $('fieldset .jq-radio').click(function(){      
       var root = $(this).parent();
       var currentIndex = root.index();              
       var group = root.parent().children().eq(currentIndex+1);    
@@ -44,6 +47,23 @@ function SForm() {
         }                                                               
       }           
     });
+
+    $('fieldset .jq-checkbox').on('click', function(){      
+      var root = $(this).parent();
+      var currentIndex = root.index();              
+      var group = root.parent().children().eq(currentIndex+1);    
+
+      if($(this).hasClass('checked')){
+        if(group.hasClass('fields_cover_inner')){
+          group.slideDown();
+        }
+      } else {
+        if(group.hasClass('fields_cover_inner')){
+          group.slideUp();
+        }        
+      }                                                              
+          
+    });    
      
     $('.jq-radio').click(function(){
       
@@ -74,7 +94,47 @@ function SForm() {
       $(this).addClass('lock');             
     });                                     
 
-    $('.jq-radio.checked').click(); 
+    $('.jq-radio.checked').click();
+
+    /*выбор детского садика*/
+    $('.map_list .jq-checkbox').on('click', function(){
+
+      if($('.map_list .jq-checkbox.checked').length){
+        $('.select_item_cover').slideDown();
+      } else {
+        $('.select_item_cover').slideUp();
+      }
+
+      if($('.map_list .jq-checkbox.checked').length==4){
+        $('.map_list .jq-checkbox:not(.checked)').addClass('disabled'); 
+      } else {
+        $('.map_list .jq-checkbox').removeClass('disabled'); 
+      }
+
+      var root = $(this).parent();
+      var currentIndex = root.parent().index();
+      var item = root.parent().find('.map_txt').text();
+      if (!$(this).hasClass('checked')){
+        $('#cfp_'+ currentIndex).remove();
+        if(!$('.select_item_list em .jq-radio.checked').length){
+
+          $('.select_item_list em:first label').click();
+        }        
+      } else {
+        $(".select_item_list").append('<em id="cfp_'+ currentIndex +'" class="wrapper l_100"><input type="radio" name="g_11" id="fp_'+ currentIndex +'"/><label for="fp_'+ currentIndex +'"><span class="pseudo">'+ item +'</span></label></em>');
+        $('input').styler();
+        if(!$('.select_item_list em .jq-radio.checked').length){
+
+          $('.select_item_list em:first label').click();
+        }
+
+      }  
+    });
+
+    $(document).on('click','.select_item_list label',function(e){
+       $('.map_ballon_block').show();
+    });  
+
 
     $(function() {
         if (window.PIE) {
@@ -93,44 +153,44 @@ function SForm() {
 // Popup
 function PopUp() {
 
-  $(document).keyup(function(d) {
+  $(document).on('keyup', function(d) {
     if (d.keyCode == 27) {
       $('.popup').hide();
-      $(document).unbind('click.event');
+      $(document).off('click');
       $('#popup_fade').remove();
     }
   });
  
-  $('.popup_link').click(function(e) {
-  
+  $('.popup_link').on('click', function(e) {
+    
     var choise = "";
   
     choise = $(this).children().attr('class');
   
     var $message = $('#'+choise);
-    $('#wrap').prepend('<div id="popup_fade"></div>');
+    
     if ($message.css('display') != 'block') {
         $message.show();
         var firstClick = true;
-        $(document).bind('click.event', function(e) {
+        $(document).on('click', function(e) {
+          $('#wrap').prepend('<div id="popup_fade"></div>');
             if (!firstClick && $(e.target).closest('.popup_cover').length == 0) {
                 $message.hide();
-                $(document).unbind('click.event');
+                $(document).off('click');
                 $('#popup_fade').remove();
             }
             $('.close_popup_x').click(function(){
               $('#'+choise).hide();
-              $(document).unbind('click.event');
+              $(document).off('click');
               $('#popup_fade').remove();
             });
             
-            $('.popup .back').click(function(){
+            $('.popup .back').click(function(e){
+              e.preventDefault();
               $('#'+choise).hide();
-              $(document).unbind('click.event');
+              $(document).off('click');
               $('#popup_fade').remove();
-            });            
-            
-            firstClick = false;
+            });             
         });
     }
     e.preventDefault();
@@ -382,28 +442,65 @@ function FieldSlide(){
     });
 
 
-    $('.btn.plus', $('.btn_add-slide')).click(function(e){
+    $('.btn.plus', $('.btn_add-slide')).on('click', function(e){
         e.preventDefault();
-        var template = $cont;
-        var count = template.length + 1;
+        var count = $cont.length + 1;
         for(var i = count; i < count+1; i++){    
-            template.eq(0).clone().addClass('filed_slide-clone').appendTo($wrap);
+            $cont.eq(0).clone().addClass('filed_slide-clone').appendTo($wrap);
           }
 
-        $wrap.find('.filed_slide').each(function(){
-          var that = $(this),
-              $close = $('.close', that);
+            function getRandomArbitary(min, max) {
+              return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
 
-          $close.on('click', function(e){
-            console.log(111);
-            e.preventDefault();
-            that.slideUp(400, function(){
-              that.remove();
-            });
-          });
+              var $inp = $('input, select', $cont);
+              function checkId(){
+                $inp.each(function(){
+                    var idRandom = $(this).attr('id', getRandomArbitary(1, 1000));
+                        return idRandom;
+                    var idNew = $(this).attr('id'),
+                        label = $(this).parents('.wrapper').find('label').attr('for', idNew);
+                        return label;
+
+
+                  if($(':radio, :checkbox')){
+                    var that = $(this),
+                        group = that.attr('name', function(i, val){
+                          return that.attr('name') + '_2';
+                        });
+
+                      that.parent().attr('id', function(i, val){
+                        return that.attr('id') + '-styler';
+                      });
+                  }
+                });
+              }
+
+              checkId();
+
+              $('input', '.filed_slide').on('click', function(){
+                $(this).styler();
+                console.log('id: ' + ($(this).attr('id')));
+              })
+
+              $('label', '.filed_slide').on('click', function(){
+                console.log('for: ' + ($(this).attr('for')));
+              })      
+
+              $wrap.find('.filed_slide').each(function(){
+                var that = $(this),
+                    $close = $('.close', that);
+
+                  $close.on('click', function(e){
+                    e.preventDefault();
+                    that.slideUp(400, function(){
+                      that.remove();
+                    });
+                });
     })
   });
 }
+
 
 function tableAsynchron(){
   var $tbl = $('.asynchron-tbl'),
